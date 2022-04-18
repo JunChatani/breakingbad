b7cb8d69c6f344d722bfac10984ba2be7b94bfb0
Jun Chatani <jun.chatani@persgroep.net>
2022-04-18 17:40:44 +0200

    Add all tests for ShowService, pass everything (19)

---
+++ src/domain/ports/FakeDrama.ts
@@ -0,0 +1,9 @@
+import {IDrama} from "./IDrama";
+import {episode} from "../model/episode";
+
+export class FakeDrama implements IDrama {
+    getEpisodes(): Promise<episode[] | null> {
+        // @ts-ignore
+        return Promise.resolve(undefined);
+    }
+}
--- src/domain/ports/ShowService.test.ts
+++ src/domain/ports/ShowService.test.ts
@@ -1,62 +1,98 @@
 import {ShowService} from "./ShowService";
 
 // Mocking should simplify this import.
-import {BreakingBadClient} from "../../adapters/breakingBadClient/BreakingBadClient";
 import clientExample from "./ClientMockResponse.json";
-import { episode } from "../model/episode";
-import { when } from "jest-when"
-
-
-// jest.mock("./../../adapters/breakingBadClient/BreakingBadClient", () => {
-    // const originalModule = jest.requireActual('./../../adapters/breakingBadClient/BreakingBadClient');
-    // let clientResponse : episode[];
-    // clientResponse = clientExample as any;
-    // return {
-        // __esModule: true,
-        // ...originalModule,
-        // getEpisodes: clientResponse
-    // };
-    // return {
-    //     default:  jest.fn( () => clientResponse)
-    // }
-// });
+import {episode} from "../model/episode";
+import {IDrama} from "./IDrama";
+import {FakeDrama} from "./FakeDrama";
 
 describe('ShowService', () => {
     let showService: ShowService;
-    // const clientResponse : episode[] = clientExample as any
-    let clientResponse : Promise<episode[]>;
+    let clientResponse: Promise<episode[]>;
     clientResponse = clientExample as any;
+
     beforeAll(() => {
-        let FakeBreakingBadClient  = new BreakingBadClient();
+        let FakeBreakingBadClient: IDrama = new FakeDrama();
         FakeBreakingBadClient.getEpisodes = jest.fn(() => clientResponse);
         showService = new ShowService(FakeBreakingBadClient);
     })
+
     it('getEpisodes for a single character by name as string', async () => {
-        // when().called().mockResolvedValue(clientResponse);
         expect((await showService.getEpisodesByName("Walter White")).length).toEqual(3);
     });
-    it('getEpisodes for a single character by name as list', async () => {
-        expect((await showService.getEpisodesByName(["Skyler White", "Ted Beneke"])).length).toEqual(2);
+
+    it('getEpisodes for a multiple character by name as list', async () => {
+        expect((await showService.getEpisodesByName(["Walter White"]))).toEqual([
+            {
+                "title": "Pilot",
+                "season": "1",
+                "characters": [
+                    "Walter White",
+                    "Jesse Pinkman",
+                    "Skyler White",
+                    "Hank Schrader",
+                    "Marie Schrader",
+                    "Walter White Jr.",
+                    "Krazy-8",
+                    "Bogdan Wolynetz"
+                ],
+                "episode": "1"
+            },
+            {
+                "title": "Ozymandias",
+                "season": "5",
+                "characters": [
+                    "Walter White",
+                    "Jesse Pinkman",
+                    "Skyler White",
+                    "Hank Schrader",
+                    "Marie Schrader",
+                    "Walter White Jr.",
+                    "Todd Alquist",
+                    "Jack Welker",
+                    "Steve Gomez"
+                ],
+                "episode": "14"
+            },
+            {
+                "title": "Kafkaesque",
+                "season": "3",
+                "characters": [
+                    "Walter White",
+                    "Jesse Pinkman",
+                    "Skyler White",
+                    "Hank Schrader",
+                    "Marie Schrader",
+                    "Walter White Jr.",
+                    "Gustavo Fring",
+                    "Saul Goodman",
+                    "Ted Beneke"
+                ],
+                "episode": "9"
+            }
+        ]);
     });
-    // it('getEpisodes for a multiple character by name as list', async () => {
-    //     expect((await showService.getEpisodesByName(["Walter White", "Skyler White"]))[0]).toEqual([
-    //         {
-    //             "title": "Ozymandias",
-    //             "season": "5",
-    //             "characters": [
-    //                 "Walter White",
-    //                 "Jesse Pinkman",
-    //                 "Skyler White",
-    //                 "Hank Schrader",
-    //                 "Marie Schrader",
-    //                 "Walter White Jr.",
-    //                 "Todd Alquist",
-    //                 "Jack Welker",
-    //                 "Steve Gomez"
-    //             ],
-    //         }
-    //     ]);
-    // });
+
+    it('getEpisodes for a single character by name as list', async () => {
+        expect((await showService.getEpisodesByName(["Skyler White", "Ted Beneke"]))).toEqual([
+            {
+                "title": "Kafkaesque",
+                "season": "3",
+                "characters": [
+                    "Walter White",
+                    "Jesse Pinkman",
+                    "Skyler White",
+                    "Hank Schrader",
+                    "Marie Schrader",
+                    "Walter White Jr.",
+                    "Gustavo Fring",
+                    "Saul Goodman",
+                    "Ted Beneke"
+                ],
+                "episode": "9"
+            }
+        ]);
+    });
 });
 
 

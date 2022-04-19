8fd0757cebb0ef145df00e1cd233dcf0a00cd28a
Jun Chatani <jun.chatani@persgroep.net>
2022-04-19 13:41:06 +0200

    Rename and refactor ShowService tests (21)

--- src/domain/ports/ShowService.test.ts
+++ src/domain/ports/ShowService.test.ts
@@ -1,19 +1,16 @@
-import { IDrama } from "./IDrama";
-import { FakeDrama } from "./FakeDrama";
-import { ShowService } from "./ShowService";
+import {IDrama} from "./IDrama";
+import {FakeDrama} from "./FakeDrama";
+import {ShowService} from "./ShowService";
 
-import { episode } from "../model/episode";
+import {episode} from "../model/episode";
 
 import clientExample from "./ClientMockResponse.json";
 
-
-// pre-allocate variable.
-let showService : ShowService;
-
 describe('Show Service: ', () => {
+    let showService: ShowService;
+
     beforeAll(() => {
-        let clientResponse: Promise<episode[]>;
-        clientResponse = clientExample as any;
+        const clientResponse: Promise<episode[]> = clientExample as any;
 
         const FakeBreakingBadClient: IDrama = new FakeDrama();
         FakeBreakingBadClient.getEpisodes = jest.fn(() => clientResponse);
@@ -22,19 +19,41 @@ describe('Show Service: ', () => {
     });
     describe('verify list of episodes in correct format are returned ', () => {
         it('for no characters passed', async () => {
-            const empty : string[] = [];
-            expect(await showService.getFormattedEpisodeListByName()).toEqual(empty);
+            // Given & When
+            const actualFormattedEpisodes = await showService.getFormattedEpisodeListByName();
+
+            // Then
+            expect(actualFormattedEpisodes).toEqual([]);
         });
         it('for no characters passed as string', async () => {
-            const empty : string[] = [];
-            expect(await showService.getFormattedEpisodeListByName("")).toEqual(empty);
+            // Given
+            const name: string = "";
+
+            // When
+            const actualFormattedEpisodes = await showService.getFormattedEpisodeListByName(name);
+
+            // Then
+            expect(actualFormattedEpisodes).toEqual([]);
         });
         it('for no characters passed as list', async () => {
-            const empty : string[] = [];
-            expect(await showService.getFormattedEpisodeListByName(empty)).toEqual(empty);
+            // Given
+            const names: string[] = [];
+
+            // When
+            const actualFormattedEpisodes = await showService.getFormattedEpisodeListByName(names);
+
+            // Then
+            expect(actualFormattedEpisodes).toEqual([]);
         });
         it('for single characters passed', async () => {
-            expect(await showService.getFormattedEpisodeListByName(['Walter White'])).toEqual(
+            // Given
+            const names: string[] = ['Walter White'];
+
+            // When
+            const actualFormattedEpisodes = await showService.getFormattedEpisodeListByName(names);
+
+            // Then
+            expect(actualFormattedEpisodes).toEqual(
                 [
                     "S0101 - Pilot",
                     "S0514 - Ozymandias",
@@ -51,7 +70,7 @@ describe('Show Service: ', () => {
     });
     describe('returns correct format when string() is called with certain episode. ', () => {
         it('with episode number and season less than 10', () => {
-            const ep : episode = {
+            const ep: episode = {
                 title: "Kafkaesque",
                 season: 3,
                 characters: [],
@@ -60,7 +79,7 @@ describe('Show Service: ', () => {
             expect(showService.formatEpisode(ep)).toEqual("S0309 - Kafkaesque");
         });
         it('with episode number greater than 9 and season less than 10', () => {
-            const ep : episode = {
+            const ep: episode = {
                 title: "Kafkaesque",
                 season: 7,
                 characters: [],
@@ -69,7 +88,7 @@ describe('Show Service: ', () => {
             expect(showService.formatEpisode(ep)).toEqual("S0712 - Kafkaesque");
         });
         it('with episode number and season greater than 9', () => {
-            const ep : episode = {
+            const ep: episode = {
                 title: "Kafkaesque",
                 season: 17,
                 characters: [],
@@ -81,55 +100,55 @@ describe('Show Service: ', () => {
     describe('verify episodes characters are present in with getEpisodeByName. ', () => {
         it('in case a single character is passed as string', async () => {
             expect((await showService.getEpisodesByName("Walter White"))).toEqual(
-            [
-                {
-                    title: "Pilot",
-                    season: "1",
-                    characters: [
-                        "Walter White",
-                        "Jesse Pinkman",
-                        "Skyler White",
-                        "Hank Schrader",
-                        "Marie Schrader",
-                        "Walter White Jr.",
-                        "Krazy-8",
-                        "Bogdan Wolynetz"
-                    ],
-                    episodeNumber: "1"
-                },
-                {
-                    title: "Ozymandias",
-                    season: "5",
-                    characters: [
-                        "Walter White",
-                        "Jesse Pinkman",
-                        "Skyler White",
-                        "Hank Schrader",
-                        "Marie Schrader",
-                        "Walter White Jr.",
-                        "Todd Alquist",
-                        "Jack Welker",
-                        "Steve Gomez"
-                    ],
-                    episodeNumber: "14"
-                },
-                {
-                    title: "Kafkaesque",
-                    season: "3",
-                    characters: [
-                        "Walter White",
-                        "Jesse Pinkman",
-                        "Skyler White",
-                        "Hank Schrader",
-                        "Marie Schrader",
-                        "Walter White Jr.",
-                        "Gustavo Fring",
-                        "Saul Goodman",
-                        "Ted Beneke"
-                    ],
-                    episodeNumber: "9"
-                }
-            ]);
+                [
+                    {
+                        title: "Pilot",
+                        season: "1",
+                        characters: [
+                            "Walter White",
+                            "Jesse Pinkman",
+                            "Skyler White",
+                            "Hank Schrader",
+                            "Marie Schrader",
+                            "Walter White Jr.",
+                            "Krazy-8",
+                            "Bogdan Wolynetz"
+                        ],
+                        episodeNumber: "1"
+                    },
+                    {
+                        title: "Ozymandias",
+                        season: "5",
+                        characters: [
+                            "Walter White",
+                            "Jesse Pinkman",
+                            "Skyler White",
+                            "Hank Schrader",
+                            "Marie Schrader",
+                            "Walter White Jr.",
+                            "Todd Alquist",
+                            "Jack Welker",
+                            "Steve Gomez"
+                        ],
+                        episodeNumber: "14"
+                    },
+                    {
+                        title: "Kafkaesque",
+                        season: "3",
+                        characters: [
+                            "Walter White",
+                            "Jesse Pinkman",
+                            "Skyler White",
+                            "Hank Schrader",
+                            "Marie Schrader",
+                            "Walter White Jr.",
+                            "Gustavo Fring",
+                            "Saul Goodman",
+                            "Ted Beneke"
+                        ],
+                        episodeNumber: "9"
+                    }
+                ]);
         });
         it('in case a single character is passed as a list', async () => {
             expect((await showService.getEpisodesByName(["Walter White"]))).toEqual(
@@ -137,24 +156,24 @@ describe('Show Service: ', () => {
         });
         it('in case multiple characters are passed as list', async () => {
             expect((await showService.getEpisodesByName(["Skyler White", "Ted Beneke"]))).toEqual(
-            [
-                {
-                    title: "Kafkaesque",
-                    season: "3",
-                    characters: [
-                        "Walter White",
-                        "Jesse Pinkman",
-                        "Skyler White",
-                        "Hank Schrader",
-                        "Marie Schrader",
-                        "Walter White Jr.",
-                        "Gustavo Fring",
-                        "Saul Goodman",
-                        "Ted Beneke"
-                    ],
-                    episodeNumber: "9"
-                }
-            ]);
+                [
+                    {
+                        title: "Kafkaesque",
+                        season: "3",
+                        characters: [
+                            "Walter White",
+                            "Jesse Pinkman",
+                            "Skyler White",
+                            "Hank Schrader",
+                            "Marie Schrader",
+                            "Walter White Jr.",
+                            "Gustavo Fring",
+                            "Saul Goodman",
+                            "Ted Beneke"
+                        ],
+                        episodeNumber: "9"
+                    }
+                ]);
         });
     });
 });

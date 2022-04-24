4219472033874ad4be0dd474324d8933624e24f8
Kemal Mahmuljin <kemalmahmuljin@hotmail.com>
2022-04-24 14:38:52 +0200

    check stage changes 9

--- api-implementation/src/adapters/api/handler.ts
+++ api-implementation/src/adapters/api/handler.ts
@@ -1,40 +1,36 @@
+// // import { BreakingBadClient } from "../../adapters/breakingBadClient/BreakingBadClient";
+// // import { ShowService } from "./ShowService";
+
+// let breakingBadShowService: ShowService;
+// let pokemShowservice
+// // function BreakingBadHandler(/*list characters/character*/) {
+// //   if (breakingBadShowService === undefined) {
+// //     showservice = new ShowService(new BreakingBadClient());
+// //   }
+// //   /*
+// //   showservice.get(list characters/character)
+// //   */
+// // }
+
+// import {
+//   Handler,
+//   APIGatewayProxyEventV2,
+//   APIGatewayProxyResultV2,
+// } from "aws-lambda";
+// import { rootLogger } from "ts-jest";
 // import { BreakingBadClient } from "../../adapters/breakingBadClient/BreakingBadClient";
 // import { ShowService } from "./ShowService";
 
+// localhost/getEpisodes{char=characters}
 
-let breakingBadShowService: ShowService;
-let pokemShowservice
-// function BreakingBadHandler(/*list characters/character*/) {
-//   if (breakingBadShowService === undefined) {
-//     showservice = new ShowService(new BreakingBadClient());
-//   }
-//   /*
-//   showservice.get(list characters/character)
-//   */
-// }
+// type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
 
-import {
-  Handler,
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResultV2,
-} from "aws-lambda";
-import { rootLogger } from "ts-jest";
-import { BreakingBadClient } from "../../adapters/breakingBadClient/BreakingBadClient";
-import { ShowService } from "./ShowService";
-
-
-localhost/getEpisodes{char=characters}
-
-
-
-type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handler: ProxyHandler = async (event, context) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: "Unknown endpoint",
-    }),
-  };
-};
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// export const handler: ProxyHandler = async (event, context) => {
+//   return {
+//     statusCode: 201,
+//     body: JSON.stringify({
+//       message: "Unknown endpoint",
+//     }),
+//   };
+// };

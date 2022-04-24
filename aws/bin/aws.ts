5affcf966abe5a3fef50698231dda758db5307c9
Kemal Mahmuljin <kemalmahmuljin@hotmail.com>
2022-04-24 13:48:30 +0200

    check stage changes 5

--- aws/bin/aws.ts
+++ aws/bin/aws.ts
@@ -3,4 +3,9 @@ import { App } from "aws-cdk-lib";
 import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new App();
-new PipelineStack(app, "PipelineStack");
+new PipelineStack(app, "PipelineStack", {
+  env: {
+    account: "047177058507",
+    region: "eu-west-1",
+  },
+});

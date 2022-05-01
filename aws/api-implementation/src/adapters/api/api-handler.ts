4ea222b1af8c9e56033e0c1eb2edbcf8e56fd429
Kemal Mahmuljin <kemalmahmuljin@hotmail.com>
2022-05-01 15:26:51 +0200

    hotswap

--- aws/api-implementation/src/adapters/api/api-handler.ts
+++ aws/api-implementation/src/adapters/api/api-handler.ts
@@ -3,9 +3,11 @@ import { BreakingBadClient } from "./../breakingBadClient/BreakingBadClient";
 
 exports.handler = async function (event: any) {
   console.log("request:", JSON.stringify(event, undefined, 2));
+  console.log("Heyy");
   console.log(event);
 
   const showService = new ShowService(new BreakingBadClient());
+  console.log("Reached ?");
 
   try {
     const something = await showService.getFormattedEpisodesByName(
@@ -15,6 +17,31 @@ exports.handler = async function (event: any) {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
       body: JSON.stringify(something),
+    };
+  } catch (errors) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "text/plain" },
+      body: `Something failed internally: ${errors}`,
+    };
+  }
+};
+
+exports.copyHandlers = async function (event: any) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  console.log(event);
+
+  const showService = new ShowService(new BreakingBadClient());
+  console.log("Copy Handler ?");
+
+  try {
+    const something = await showService.getFormattedEpisodesByName(
+      event.characters
+    );
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify("Hallo"),
     };
   } catch (errors) {
     return {
